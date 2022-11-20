8a48cad20043fd5352eea7f35e5692e3614da0d1
Hazama <hafizamin8116@gmail.com>
2022-11-20 18:25:16 +0700

    dashboard

--- src/components/Dashboard/DashboardTableRow.jsx
+++ src/components/Dashboard/DashboardTableRow.jsx
@@ -1,34 +1,57 @@
-export const DashboardTableRow = ({title}) => {
-  return (
-    <div className="row row-content">
-      <div className="col d-flex align-items-center justify-content-center">
-        {title}
-      </div>
-      <div className="col d-flex align-items-center justify-content-center">
-        500
-      </div>
-      <div className="col d-flex flex-col align-items-center justify-content-center">
-        <div className="row py-2">
-          <div className="col">500</div>
+export const DashboardTableRow = ({ dashboardItem }) => {
+    const formatter = new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+    });
+
+    return (
+        <div className="row row-content">
+            <div className="col d-flex align-items-center justify-content-center">
+                {dashboardItem.name}
+            </div>
+            <div className="col d-flex align-items-center justify-content-center">
+                <div className="row py-2">
+                    {dashboardItem.total_tanah_induk}
+                </div>
+            </div>
+            <div className="col d-flex flex-col align-items-center justify-content-center">
+                <div className="row py-2">
+                    <div className="col">
+                        {dashboardItem.total_tanah_pinjam_pakai}
+                    </div>
+                </div>
+            </div>
+            <div className="col d-flex flex-col align-items-center justify-content-center">
+                <div className="row py-2">
+                    <div className="col">
+                        {dashboardItem.total_tanah_pakai_sendiri}
+                    </div>
+                </div>
+            </div>
+            <div className="col d-flex flex-col align-items-center justify-content-center">
+                <div className="row py-2">
+                    <div className="col">
+                        {dashboardItem.total_tanah_sewa_sip_bmd}
+                    </div>
+                    <div className="col">
+                        {formatter.format(
+                            dashboardItem.total_rupiah_tanah_sewa_sip_bmd
+                        )}
+                    </div>
+                </div>
+            </div>
+            <div className="col d-flex flex-col align-items-center justify-content-center">
+                <div className="row py-2">
+                    <div className="col">
+                        {dashboardItem.total_tanah_retribusi}
+                    </div>
+                    <div className="col">
+                        {formatter.format(
+                            dashboardItem.total_rupiah_tanah_retribusi
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
-      </div>
-      <div className="col d-flex flex-col align-items-center justify-content-center">
-        <div className="row py-2">
-          <div className="col">500</div>
-        </div>
-      </div>
-      <div className="col d-flex flex-col align-items-center justify-content-center">
-        <div className="row py-2">
-          <div className="col">500</div>
-          <div className="col">5.000.000</div>
-        </div>
-      </div>
-      <div className="col d-flex flex-col align-items-center justify-content-center">
-        <div className="row py-2">
-          <div className="col">500</div>
-          <div className="col">5.000.000</div>
-        </div>
-      </div>
-    </div>
-  );
+    );
 };
